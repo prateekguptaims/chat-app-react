6a01528cb71b72940375c27eb95065cf56d23fd6
Prateek <56671784+prateekguptaims@users.noreply.github.com>
2024-10-18 16:43:48 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5174", 
+        origin: "https://chatappreact-three.vercel.app/", 
         methods: ["GET","POST","PUT"]
     }
 });

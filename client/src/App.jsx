9158fed98f4701498e0bbe7a46161d8e27bee88f
Prateek <56671784+prateekguptaims@users.noreply.github.com>
2024-10-19 11:56:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,6 @@ import io from 'socket.io-client'
 import './App.css'
 import JoinChatPage from './join_room'
 
-const socket=io.connect("https://chatbackend-sage.vercel.app")
-
 
 function App() {
 
